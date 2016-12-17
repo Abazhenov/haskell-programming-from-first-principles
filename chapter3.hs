b32e75e51a2fdb746f0e39fab960b9e022eface6
Alexei Bazhenov <abazhenov@gmail.com>
2016-12-16 19:12:20 -0600

    init

---
+++ chapter3.hs
@@ -0,0 +1,11 @@
+-- module Pring1 where
+--   main :: IO()
+--   main = putStrLn "hello world!"
+
+module Prin2 where
+  main :: IO ()
+  main = do
+    putStrLn "Count to four for me:"
+    putStr "one, two"
+    putStr ", three, and"
+    putStrLn " four!"
